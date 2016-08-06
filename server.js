1220c1a5f4cb3b6cc929cd3eef8a8134068035e5
Louis Lo <LLo1@users.noreply.github.com>
2016-08-06 15:23:19 -0700

    (feat) Configure for Heroku deployment (#16)
    * (fix) Update port retrieval for Heroku
    
    * (chore) Configure app for Heroku Deployment

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 const helpers = require('./routes/routesHelper');
 
-app.set('port', (process.env.API_PORT || 3001));
+var port = process.env.PORT || 3001;
+
 require('./config/middleware')(app, express);
 
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+app.listen(port, () => {
+  console.log(`Server on port: ${port}/`);
 });
