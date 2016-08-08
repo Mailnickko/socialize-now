43e33ca02a16a351ed032c899e610a50873c5ae5
Nicholas Ko <mailnickko@gmail.com>
2016-08-07 22:37:48 -0700

    (refactor) Change filename from routes to router

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/presentational/App';
-import Login from './component/presentational/Login';
+import Login from './components/presentational/Login';
 
 export default (
   <Route path='/' component={ App }>
