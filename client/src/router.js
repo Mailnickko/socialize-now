7fbec88f1c9988a4cab3978f87a86d092ec86929
Nicholas Ko <mailnickko@gmail.com>
2016-08-08 13:48:03 -0700

    (feat) Will add dashboard page to routes

--- client/src/router.js
+++ client/src/router.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/presentational/App';
 import Login from './components/presentational/Login';
+import Dashboard from './components/presentational/Dashboard'
 
 export default (
   <Route path='/' component={ App }>
     <IndexRoute component={ Login } />
+    <Route path='/dashboard' component={ Dashboard } />
   </Route>
 );
