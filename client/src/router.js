848816b36adf4385e07d0edcb61472a4e487b33a
Nicholas Ko <mailnickko@gmail.com>
2016-08-08 14:44:40 -0700

    (feat) Add basic component structure for Nominations Page

--- client/src/router.js
+++ client/src/router.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/presentational/App';
 import Login from './components/presentational/Login_Page';
 import Dashboard from './components/presentational/Dashboard_Page'
+import Nominations from './components/presentational/Nominations_Page';
 
 export default (
   <Route path='/' component={ App }>
     <IndexRoute component={ Login } />
     <Route path='/dashboard' component={ Dashboard } />
+    <Route path='/nominations' component={ Nominations } />
   </Route>
 );
