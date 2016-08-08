ea32ae2841c81153093c84996d35df22a2ace5dc
Nicholas Ko <mailnickko@gmail.com>
2016-08-08 15:07:24 -0700

    (feat) Add component for Winning Vote

---
+++ client/src/components/presentational/WinningResult.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../../styles/css/temp.css';
+
+class WinningResult extends Component {
+
+  render() {
+    return (
+      <div>
+        <div className="center">
+          <h1>Winning Result title</h1>
+          <h2>Winning Result Info</h2>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default WinningResult;
