96a09cf96e03868cdae2550cc62aa5ef33d4cbf6
Nicholas Ko <mailnickko@gmail.com>
2016-08-08 13:51:46 -0700

    (feat) Will mount redux form in reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as form } from 'redux-form';
 import participantsReducer from './reducer_participants';
 import nomineesReducer from './reducer_nominees';
 import suggestionsReducer from './reducer_suggestions';
 
 // Combine results from indiviual reducers into a single rooteReducer to be used in the store
 const rootReducer = combineReducers({
+  form,
   participants: participantsReducer,
   nominees: nomineesReducer,
   suggestions: suggestionsReducer,
