e3d37c48a58849e78a700f77a5d8cf027e58f9c6
Nicholas Ko <mailnickko@gmail.com>
2016-08-08 12:49:17 -0700

    (feat) Add foundation for Dashboard and NewEvent

---
+++ client/src/components/presentational/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../../styles/css/temp.css';
+
+//think of this as simply a wraper for the dashboard page
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+};
+
+export default Dashboard;
