64e25cb0db4b79b7d262e3947759be5b71c4fa8a
Nicholas Ko <mailnickko@gmail.com>
2016-08-07 19:29:12 -0700

    (refactor) Organize and modify files to serve vanilla create-react-app landing page

--- client/src/components/presentational/App.js
+++ client/src/components/presentational/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../styles/images/logo.svg';
+import '../../styles/css/App.css';
 
 class App extends Component {
   render() {
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/presentational/App';
+import './styles/css/index.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementsByClassName('root')[0]
+  document.querySelector('.root')
 );
