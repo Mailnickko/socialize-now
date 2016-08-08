8cf054ad6b41d21b9c88fad2747f64748f420d42
Nicholas Ko <mailnickko@gmail.com>
2016-08-07 20:59:58 -0700

    (feat) Add Login component

---
+++ client/src/components/presentational/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../../styles/css/temp.css';
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div>
+        <div className="center">
+          <h1>Landing Page Header</h1>
+          <button>Signin/Register</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/presentational/App';
+import Login from './component/presentational/Login';
 
 export default (
-  <Route path='/' component={App}>
-
+  <Route path='/' component={ App }>
+    <IndexRoute component={ Login } />
   </Route>
 );
