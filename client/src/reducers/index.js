f895fdd284e2613b18f0827f4542d6cb806c5296
Nicholas Ko <mailnickko@gmail.com>
2016-08-08 16:11:12 -0700

    (feat) Include reducer for dummy data

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,12 @@ import { reducer as form } from 'redux-form';
 import participantsReducer from './reducer_participants';
 import nomineesReducer from './reducer_nominees';
 import suggestionsReducer from './reducer_suggestions';
+import singleUserReducer from './reducer_singleUser';
 
 // Combine results from indiviual reducers into a single rooteReducer to be used in the store
 const rootReducer = combineReducers({
   form,
+  singleUser: singleUserReducer,
   participants: participantsReducer,
   nominees: nomineesReducer,
   suggestions: suggestionsReducer,
