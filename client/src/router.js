05ae1a15874792389316348a780da6eadb72df31
Nicholas Ko <mailnickko@gmail.com>
2016-08-08 14:03:26 -0700

    (style) Change file names for clarity

--- client/src/router.js
+++ client/src/router.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/presentational/App';
-import Login from './components/presentational/Login';
-import Dashboard from './components/presentational/Dashboard'
+import Login from './components/presentational/Login_Page';
+import Dashboard from './components/presentational/Dashboard_Page'
 
 export default (
   <Route path='/' component={ App }>
