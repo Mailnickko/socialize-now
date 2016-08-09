c24835b723fe1774bc19b58063785e182334c4f3
Nicholas Ko <mailnickko@gmail.com>
2016-08-08 17:16:46 -0700

    (fix) Stripped all Redux-forms features and added temp workaround

--- client/src/components/containers/Dashboard_NewVote.js
+++ client/src/components/containers/Dashboard_NewVote.js
@@ -1,77 +1,46 @@
 import React, { Component } from 'react';
 import '../../styles/css/temp.css';
-import { reduxForm } from 'redux-form';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import * as actionCreators from '../../actions/actionCreators';
 
 class NewEvent extends Component {
 
-  handleFormSubmit(formProps) {
-    //Fire off an action creator
+  constructor(props) {
+    super(props);
   }
 
-    //This will become better implemented once we have a connection to the back end
-  renderAlert() {
-    if (this.props.hasError) {
-      return (
-        <div>
-          <strong>{ this.props.hasError }</strong>
-        </div>
-      )
-    }
+  //Hate to do it this way..very hacky but redux forms is giving us some trouble
+    //This should work for no until we come up with a better solution
+  makeEvent(e) {
+    e.preventDefault();
+    let sendObj = {
+      date: e.target.date.value,
+      invitees: e.target.invitedUsers.value.split(','),
+      priceRange: e.target.priceRange.value
+    };
+
+    //would call an action creator at this point using this data
   }
 
-  //State here will be handled by redux forms. Once again, will become possible to implement with connection to back end
   render() {
-    const { handleSubmit, fields: { date, invitedUsers, priceRange } } = this.props;
-
     return (
-      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <fieldset>
+      <form onSubmit={this.makeEvent.bind(this)}>
           <label>Date:</label>
-          <input type="date" { ...date.input } />
-          {date.touched && date.error && <div className="error">{date.error}</div>}
-        </fieldset>
-        <fieldset>
+          <input type="date" placeholder="Date" name="date" />
           <label>Invited Users:</label>
-          <input type="text" { ...invitedUsers.input } />
-          {invitedUsers.touched && invitedUsers.error && <div className="error">{invitedUsers.error}</div>}
-        </fieldset>
-        <fieldset>
+          <input type="text" name="invitedUsers" />
           <label>Price Range:</label>
-          <div>
-            <input type="radio" { ...priceRange.input } value="10" checked={priceRange.value === "10"} /> $
-          </div>
-          <div>
-            <input type="radio" { ...priceRange.input } value="30" checked={priceRange.value === "30"} /> $$
-          </div>
-          <div>
-            <input type="radio" { ...priceRange.input } value="100" checked={priceRange.value === "100"} /> $$$
-          </div>
-          {priceRange.touched && priceRange.error && <div className="error">{priceRange.error}</div>}
-        </fieldset>
-        { this.renderAlert() }
+          <select name="priceRange">
+            <option value="10">$</option>
+            <option value="30">$$</option>
+            <option value="100">$$$</option>
+          </select>
         <button action="submit">Create Event</button>
       </form>
     );
   }
 };
-
-function validate(formProps) {
-  const errors = {};
-
-  if (!formProps.date) {
-    errors.date = 'Please enter a date';
-  }
-
-  if (!formProps.invitedUsers) {
-    errors.invitedUsers = 'Events are more fun with more people';
-  }
-
-  if (!formProps.priceRange) {
-    errors.priceRange = 'Please select a price range';
-  }
-  return errors;
-}
 
 function mapStateToProps(state) {
     //Not sure what kind of error we'd return here
@@ -80,7 +49,9 @@ function mapStateToProps(state) {
   }
 }
 
-export default reduxForm({
-  form: 'newEvent',
-  fields: ['date', 'invitedUsers', 'priceRange'], validate
-}, mapStateToProps, actionCreators)(NewEvent);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewEvent);
+
