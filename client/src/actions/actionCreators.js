3bdabe31ba0d16f66651d2eca8e804c127d51f62
Nicholas Ko <mailnickko@gmail.com>
2016-08-09 22:45:12 -0700

    (feat) Add new action cretors and types

--- client/src/actions/actionCreators.js
+++ client/src/actions/actionCreators.js
@@ -20,8 +20,9 @@ export function getMessages(eventId){
 
 //Grab all Events for a user
   //Assuming here that we're getting an array of objects
+    //Will populate the List of Events in Dashboard Page
 export function grabUserEvents(userId) {
-  const userEvents = axios.get(`/user?id=${userId}`)
+  const userEvents = axios.get(`/getUserEvents/${userId}`)
     .then(function(events) {
       return events;
     })
@@ -35,4 +36,36 @@ export function grabUserEvents(userId) {
   };
 }
 
->>>>>>> (feat) Add axios and basic action creator
+//grab user data by username. most likely retrieved from JWT
+  //could also be subbed out to search by userID in the future
+    //Will populate the header in Dashboard Page
+export function grabUserInfo(username) {
+  const userInfo = axios.get(`/getUserInfo/${username}`)
+    .then(function(user) {
+      return user;
+    })
+    .catch(function(err) {
+      return err;
+    })
+
+  return {
+    type: types.GET_USER_INFO,
+    payload: userInfo
+  };
+}
+
+//Create a new Event in the DB
+  // Should expect a returned copy of the created Event Object
+    //Might not even be necessary for this to be an action creator
+      //Since the componentWillMount() will do another get req to DB
+export function createNewEvent(newEventObj) {
+  const newEvent = axios.post('/createNewEvent', newEventObj)
+    .then(function(event) {
+      return event;
+    })
+  return {
+    type: types.CREATE_NEW_EVENT,
+    payload: newEvent
+  };
+}
+
