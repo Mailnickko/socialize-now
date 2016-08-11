257aeeb372551ced917505650d9f911b05737015
jtlong3rd <jtlong3rd@gmail.com>
2016-08-10 22:38:41 -0400

    (refactor) Use object destructuring to simplify imports for neuralHelpers.js

--- consultationHelpers/neuralHelpers.js
+++ consultationHelpers/neuralHelpers.js
@@ -1,11 +1,5 @@
 const _ = require('lodash');
-const synaptic = require('synaptic');
-
-const Neuron = synaptic.Neuron;
-const Layer = synaptic.Layer;
-const Network = synaptic.Network;
-const Trainer = synaptic.Trainer;
-const Architect = synaptic.Architect;
+const { Architect } = require('synaptic');
 
 // Output: A flattened array consisting of all the tags from all users' events
 // Input: Same as the input to consultNetwork
@@ -26,9 +20,10 @@ const getTagMap = (allTags) => {
 // Input: An array of tags and an associated 'master list' of tags
 // Example: (['Chinese', Movies'], ['French', 'Movies', 'Chinese']) -> [0, 1, 1]
 const tagsToVector = (tags, tagMap) => {
-  return tagMap.reduce((memo, nextTag) => {
-    return tags.indexOf(nextTag) >= 0 ? [...memo, 1] : [...memo, 0];
-  }, []);
+  return tagMap.reduce(
+    (memo, nextTag) => [...memo, tags.indexOf(tags, nextTag) >= 0 ? 1 : 0],
+    []
+  );
 };
 
 // Output: An array of tags corresponding to the input
@@ -48,13 +43,18 @@ const vectorToTags = (vector, tagMap) => {
 // Input: The desired length of the random vector
 // Example: 3 -> [0, 1, 1]
 const randomVector = (n) => {
-  return Array.from(Array(n)).reduce((memo) => [...memo, Math.round(Math.random())], []);
+  return Array.from(Array(n)).reduce(
+    (memo) => [...memo, Math.round(Math.random())],
+    []
+  );
 };
 
-// Output: An array of arrays of tags, each of which represents a 'suggestion' based on the provided user events
-// Input: An array of user events (see examples.js), as well as the number of suggestions to return
+// Output: An array of arrays of tags, each of which represents
+// a 'suggestion' based on the provided user events
+// Input: An array of user events (see examples.js), as well as
+// the number of suggestions to return
 const consultNetwork = (userEvents, n = 1) => {
-  // Converts the userEvents into training data
+  // Convert the userEvents into training data
   const allTags = getAllTags(userEvents);
   const tagMap = getTagMap(allTags);
   const trainingDataUnflattened = userEvents.map(userEvent => {
@@ -62,14 +62,15 @@ const consultNetwork = (userEvents, n = 1) => {
   });
   const trainingData = _(trainingDataUnflattened).flatten().value();
 
-  // Creates and train a neural network
+  // Create and train a neural network
   const newNetwork = new Architect.Hopfield(tagMap.length);
   newNetwork.learn(trainingData);
 
-  // Randomly chooses n input vectors
+  // Randomly choose n input vectors
   const inputVectors = _.range(n).map(() => randomVector(tagMap.length));
 
-  // Feed the input vectors to the neural network and converts the outputs to an array of tags,
+  // Feed the input vectors to the neural network and
+  // converts the outputs to an array of tags,
   // which is then returned
   return inputVectors.map(inputVector => vectorToTags(newNetwork.feed(inputVector), tagMap));
 };
