0b29efef045ec102ea8eb9606119b8c7a5177900
Nicholas Ko <mailnickko@gmail.com>
2016-08-11 18:13:17 -0700

    (feat) Integrate HOC for auth

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import { syncHistoryWithStore} from 'react-router-redux';
+import { syncHistoryWithStore, routerActions, routerMiddleware } from 'react-router-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import promise from 'redux-promise';
+import { UserAuthWrapper } from 'redux-auth-wrapper';
 
 //From App
 import rootReducer from './reducers';
@@ -21,6 +22,16 @@ import activeUser from './data/activeUser';
 import chat from './data/chat';
 import voteStatus from './data/voteStatus';
 
+//options for authentication
+export const UserIsAuthenticated = UserAuthWrapper({
+  authSelector: state => state.user,  //get user state
+  predicate: auth => auth.isAuthenticated,  //checks result of authSelector, if false, redirect
+  redirectAction: routerActions.replace,   //redux action creator to handle redirect
+  wrapperDisplayName: 'UserIsAuthenticated',   //name for auth check
+  failureRedirectPath: '/',   //send unauthenticated users here
+  allowRedirectBack: false    //don't send redirect query param to failureRedirectPath
+});
+
 const defaultState = {
   participants,
   nominees,
@@ -32,8 +43,10 @@ const defaultState = {
 
 // Instantiate Store with data from rootReducer
 // const store = createStore(rootReducer, defaultState);
+// Need to include routerMiddleware for react-router-redux action creators
+const routingMiddleware = routerMiddleware(browserHistory);
 
-const store = createStore(rootReducer, defaultState, applyMiddleware(thunkMiddleware, promise));
+const store = createStore(rootReducer, defaultState, applyMiddleware(thunkMiddleware, promise, routingMiddleware));
 
 // Include state to passed along with routes
 const history = syncHistoryWithStore(browserHistory, store);
--- client/src/router.js
+++ client/src/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRedirect } from 'react-router';
+import { Authenticated } from './index';
 import App from './components/presentational/App';
 import Login from './components/containers/Login_Page';
 import Dashboard from './components/presentational/Dashboard_Page'
@@ -7,8 +8,11 @@ import Polling from './components/presentational/Polling_Page';
 
 export default (
   <Route path='/' component={ App }>
-    <IndexRoute component={ Login } />
-    <Route path='/dashboard' component={ Dashboard } />
-    <Route path='/polling' component={ Polling } />
+    <IndexRedirect to="/login" />
+    <Route path='/login' component={ Login } />
+    <Route component={Authenticated}>
+      <Route path='/dashboard' component={ Dashboard } />
+      <Route path='/polling' component={ Polling } />
+    </Route>
   </Route>
 );
