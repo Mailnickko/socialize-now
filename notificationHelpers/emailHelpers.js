8cb95fa118fad246a120da39a417ff5b7c593d09
jtlong3rd <jtlong3rd@gmail.com>
2016-08-15 19:03:39 -0400

    (refactor) Alter sendNotification to receive only one invitee's email address. (This allows the frontend to customize the body for individual invitees.)

--- notificationHelpers/emailHelpers.js
+++ notificationHelpers/emailHelpers.js
@@ -2,27 +2,25 @@ const sendGrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
 const mailHelper = require('sendgrid').mail;
 
 // Output: None
-// Side Effects: Sends an email notification out to each invitee
-// Input: The event creator, an array of all the invitees,
+// Side Effects: Sends an email notification to an invitee
+// Input: The event creator and invitee email addresses,
 // the subject, and the email body
-// Note: The creator and invitees are represented as a "User" object
-const sendNotification = (creator, invitees, subject, body) => {
-  invitees.forEach(invitee => {
-    from = new mailHelper.Email(creator.email);
-    to = new mailHelper.Email(invitee.email);
-    content = new mailHelper.Content('text/plain', body);
-    mail = new mailHelper.Mail(from, subject, to, content);
+const sendNotification = (creator, invitee, subject, body) => {
+  from = new mailHelper.Email(creator);
+  to = new mailHelper.Email(invitee);
+  content = new mailHelper.Content('text/plain', body);
+  mail = new mailHelper.Mail(from, subject, to, content);
 
-    const request = sendGrid.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: mail.toJSON()
-    });
+  const request = sendGrid.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
 
-    sendGrid.API(request, (error, response) => {
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-    });
+  sendGrid.API(request, (error, response) => {
+    //This can be uncommented for testing purposes
+    //console.log(response.statusCode);
+    //console.log(response.body);
+    //console.log(response.headers);
   });
 };
