c62362015869f65223d3364837a682185a2f9933
llo1 <louislo@gmail.com>
2016-08-15 12:26:23 -0700

    (refactor) Add token creation to before hook

--- test/endpointsSpec.js
+++ test/endpointsSpec.js
@@ -2,13 +2,18 @@ import Event from '../db/models/Event';
 // import { expect } from 'chai';
 import * as eventController from '../controllers/eventController';
 import supertest from 'supertest';
-import { app } from '../server'
+import { app } from '../server';
 
 const server = supertest.agent(app);
 
 describe('API endpoints:', () => {
+  let jwt
 
-  it(" Get / should respond with html", done => {
+  before(() => {
+    jwt = new Buffer('Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3NvY2FsaXplaHIuYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTEzNjk3ODQ4MTgyNTA2ODgxMDAxIiwiYXVkIjoiOWgxQ2dUNVZqc1hvVU9BZms2ZDRSQWo1WEMwRU84QW4iLCJpYXQiOjE0NzEyNDI0ODl9.ZZ3iFKS8Mfr-AWjFBrG-e9MeZYIXDVpgqup-JiA4BP8')
+  });
+
+  it("Get / should respond with html", done => {
     server
       .get("/")
       .expect(200)
@@ -19,7 +24,7 @@ describe('API endpoints:', () => {
       })
   });
 
-  it(" Get catch-all route should respond with html", done => {
+  it("Get catch-all route should respond with html", done => {
     server
       .get("/dfsal987")
       .expect(200)
@@ -30,9 +35,10 @@ describe('API endpoints:', () => {
       })
   });
 
-  xit(" Get /events should respond with events", done => {
+  it("Get /events should respond with events", done => {
     server
       .get("/events")
+      .set('Authorization', jwt)
       .expect(200)
       .expect('Content-Type', /json/)
       .expect(res => {
@@ -44,5 +50,4 @@ describe('API endpoints:', () => {
         done()
       })
   });
-
 });
