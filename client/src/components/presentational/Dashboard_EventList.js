44229d7c2c165e803a4b2246472cb4a2b9e67fb4
Minyens <xminhhainguyen@gmail.com>
2016-08-15 18:55:36 -0400

    (feat) Make events on dashboard redirect to unique event

--- client/src/components/presentational/Dashboard_EventList.js
+++ client/src/components/presentational/Dashboard_EventList.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 import '../../styles/css/dashboard.css';
 
 class EventList extends Component {
 
+  viewEvent(userEvent){
+    browserHistory.push(`/polling/${userEvent._id}`);
+  }
+
   render() {
     const { userEvent } = this.props;
     return (
-      <div className="eventItem">
+      <div onClick={this.viewEvent.bind(this, userEvent)} className="eventItem">
         <div className="eventContent">
-          <div className="eventHeader">{ userEvent.name }</div>
+          <div className="eventHeader">{ userEvent.name }, { userEvent._id }</div>
         </div>
       </div>
     );
