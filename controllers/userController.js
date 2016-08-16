9013fbe33bdf1c7b4aac0a5749c3a99d57fdd71b
llo1 <louislo@gmail.com>
2016-08-15 18:14:03 -0700

    (refactor) Add find or create functionality to createUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,19 @@
 const User = require('../db/models/User');
 const db = require('../db/config');
 
+
+//Checks to see if user exists
+  //if not, creates a new user and returns promise
+  //if user exists, returns user object in a promise
 module.exports.createUser = (userId, picture, email, name) => {
-  return User.create({ userId, picture , email, name});
+  User.findOne({userId})
+    .then( user => {
+      if(!user){
+        return User.create({ userId, picture , email, name});
+      } else {
+        return user;
+      }
+    });
 };
 
 module.exports.deleteUser = userId => {
