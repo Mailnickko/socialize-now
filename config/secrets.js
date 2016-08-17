e538b8a2e0de31e041bad43f47a9f4a6a0858864
llo1 <louislo@gmail.com>
2016-08-17 14:56:26 -0700

    (chore) Add config/secrets.js

--- config/secrets.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  auth0Client: '9h1CgT5VjsXoUOAfk6d4RAj5XC0EO8An',
-  auth0Domain: 'socalizehr.auth0.com',
-  jwtSecret: 'PfzP8e9QpongLs5Z6zXJszW814O8UdAakjN3V4iiWN2XH3atUvSRPU4sMWc9JI3r'
-}
