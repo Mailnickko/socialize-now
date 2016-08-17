6bac70058113969bba5a8be2095320d1336e31c7
Nicholas Ko <mailnickko@gmail.com>
2016-08-17 09:04:34 -0700

    (test) Add tests for reducers

--- test/actionCreatorsSpec.js
+++ test/actionCreatorsSpec.js
@@ -71,9 +71,53 @@ describe('action creators', () => {
     });
   });
 
+  describe('the getUserStatus action creator', () => {
+    it('should be a function', () => {
+      expect(actions.getUserStatus).to.be.a.function;
+    });
+    it('should have the correct type', () => {
+      let action = actions.getUserStatus();
+      expect(action.type).to.equal(types.GET_USER_STATUS);
+    });
+  });
+
+  describe('the getParticipants action creator', () => {
+    it('should be a function', () => {
+      expect(actions.getParticipants).to.be.a.function;
+    });
+    it('should return a dispatch function', () => {
+      let action = actions.getParticipants();
+      expect(action).to.be.a.function;
+    });
+  });
+
   describe('the createNewEvent action creator', () => {
     it('should be a function', () => {
       expect(actions.createNewEvent).to.be.a.function;
+    });
+    it('should return a dispatch function', () => {
+      let action = actions.createNewEvent();
+      expect(action).to.be.a.function;
+    });
+  });
+
+  describe('the getEvent action creator', () => {
+    it('should be a function', () => {
+      expect(actions.getEvent).to.be.a.function;
+    });
+    it('should return a dispatch function', () => {
+      let action = actions.getEvent();
+      expect(action).to.be.a.function;
+    });
+  });
+
+  describe('the inviteUser action creator', () => {
+    it('should be a function', () => {
+      expect(actions.inviteUser).to.be.a.function;
+    });
+    it('should return a dispatch function', () => {
+      let action = actions.inviteUser();
+      expect(action).to.be.a.function;
     });
   });
 
@@ -81,17 +125,31 @@ describe('action creators', () => {
     it('should be a function', () => {
       expect(actions.startVote).to.be.a.function;
     });
-  });
-
-  describe('the setWinningResult action creator', () => {
-    it('should be a function', () => {
-      expect(actions.setWinningResult).to.be.a.function;
+    it('should return a dispatch function', () => {
+      let action = actions.startVote();
+      expect(action).to.be.a.function;
     });
   });
+
+  describe('the endVote action creator', () => {
+    it('should be a function', () => {
+      expect(actions.endVote).to.be.a.function;
+    });
+    it('should return a dispatch function', () => {
+      let action = actions.endVote();
+      expect(action).to.be.a.function;
+    });
+  });
+
+  // Should wait on these because they are likely to change
 
   describe('the increaseVote action creator', () => {
     it('should be a function', () => {
       expect(actions.increaseVote).to.be.a.function;
+    });
+    it('should return a dispatch function', () => {
+      let action = actions.increaseVote();
+      expect(action).to.be.a.function;
     });
   });
 
@@ -99,11 +157,19 @@ describe('action creators', () => {
     it('should be a function', () => {
       expect(actions.decreaseVote).to.be.a.function;
     });
+    it('should return a dispatch function', () => {
+      let action = actions.decreaseVote();
+      expect(action).to.be.a.function;
+    });
   });
 
   describe('the userLogout action creator', () => {
     it('should be a function', () => {
       expect(actions.userLogout).to.be.a.function;
+    });
+    it('should have the correct type', () => {
+      let action = actions.userLogout();
+      expect(action.type).to.equal(types.LOGOUT_SUCCESS);
     });
   });
 
@@ -111,11 +177,19 @@ describe('action creators', () => {
     it('should be a function', () => {
       expect(actions.userLoginSuccess).to.be.a.function;
     });
+    it('should return a dispatch function', () => {
+      let action = actions.userLoginSuccess();
+      expect(action).to.be.a.function;
+    });
   });
 
   describe('the userLoginError action creator', () => {
     it('should be a function', () => {
       expect(actions.userLoginError).to.be.a.function;
+    });
+    it('should have the correct type', () => {
+      let action = actions.userLoginError();
+      expect(action.type).to.equal(types.LOGIN_ERROR);
     });
   });
 });
