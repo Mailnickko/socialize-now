59cc60cf2f41ea3fbeabebd27c04beabb9ca06ab
Nicholas Ko <mailnickko@gmail.com>
2016-08-17 23:58:23 -0700

    (refactor) Remove call to dummy reducer

--- client/src/reducers/dummy_reducer_nominees.js
+++ client/src/reducers/dummy_reducer_nominees.js
@@ -2,20 +2,20 @@
 import * as types from '../actions/actionTypes';
 
 function nominees(state=[], action) {
-  let i = action.index;
+  // let i = action.index;
   switch(action.type) {
-    case types.INCREASE_VOTE:
-      return [
-        ...state.slice(0, i),
-        { ...state[i], netVotes: state[i].netVotes + 1 },
-        ...state.slice(i+1)
-      ]
-    case types.DECREASE_VOTE:
-      return [
-        ...state.slice(0, i),
-        { ...state[i], netVotes: state[i].netVotes - 1 },
-        ...state.slice(i+1)
-      ]
+    // case types.INCREASE_VOTE:
+    //   return [
+    //     ...state.slice(0, i),
+    //     { ...state[i], netVotes: state[i].netVotes + 1 },
+    //     ...state.slice(i+1)
+    //   ]
+    // case types.DECREASE_VOTE:
+    //   return [
+    //     ...state.slice(0, i),
+    //     { ...state[i], netVotes: state[i].netVotes - 1 },
+    //     ...state.slice(i+1)
+    //   ]
     default:
       return state;
   }
