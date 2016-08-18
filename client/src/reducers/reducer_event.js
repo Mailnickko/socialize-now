73ded04fca13dd654be11d8b50863050b31dc4ad
Nicholas Ko <mailnickko@gmail.com>
2016-08-17 23:58:56 -0700

    (feat) Update reducer to make proper state changes

--- client/src/reducers/reducer_event.js
+++ client/src/reducers/reducer_event.js
@@ -1,14 +1,21 @@
 import * as types from '../actions/actionTypes';
 
 function event(state={}, action) {
+  let i = action.index;
+  let currentChoices = state.choices;
   switch (action.type){
-
     case types.FIND_EVENT:
       return action.payload || state;
     case types.START_VOTING:
-      return {...state, isVoting: true};
+      return action.payload;
     case types.END_VOTING:
-      return {...state, voteCompleted: true, choice: [...state, action.payload]};
+      return action.payload;
+    case types.INCREASE_VOTE:
+      currentChoices[i].netVotes++;
+      return {...state, choices: currentChoices };
+    case types.DECREASE_VOTE:
+      currentChoices[i].netVotes--;
+      return {...state, choices: currentChoices };
     default:
       return state;
   }
