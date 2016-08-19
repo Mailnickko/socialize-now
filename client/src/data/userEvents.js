f9eaef33616dfa8159f55c3f276ff1f130ef6d77
Minyens <xminhhainguyen@gmail.com>
2016-08-19 17:52:31 -0400

    (Feat) Add dummy data for userEvents for CSS

---
+++ client/src/data/userEvents.js
@@ -0,0 +1,48 @@
+const ramen = {
+  imageURL: "https://s3-media3.fl.yelpcdn.com/bphoto/IhfaY4tDZcd_lM5LEeSYjw/ms.jpg",
+  name: "Tosh's Ramen",
+  netVotes: 1,
+  rating: 4,
+  ratingImg: "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/c2f3dd9799a5/ico/stars/v1/stars_4.png",
+  reviewCount: 302,
+  url: "http://www.yelp.com/biz/toshs-ramen-salt-lake-city-2?adjust_creative=e-QBkPVuSWpweMbvJ7XxnA&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=e-QBkPVuSWpweMbvJ7XxnA"
+}
+
+const userEvents = [{
+  _id: "test",
+  choice: [ramen],
+  choices: [ramen, ramen, ramen, ramen, ramen, ramen],
+  creator: "test",
+  date: "2016-08-20",
+  isVoting: true,
+  name: "test",
+  time: "01:00",
+  users: [],
+  voteCompleted: true
+},
+{
+  _id: "test voting",
+  choice: [ramen],
+  choices: [ramen, ramen, ramen, ramen, ramen, ramen],
+  creator: "test",
+  date: "2016-08-20",
+  isVoting: true,
+  name: "test",
+  time: "01:00",
+  users: [],
+  voteCompleted: false
+},
+{
+  _id: "test lobby",
+  choice: [ramen],
+  choices: [ramen, ramen, ramen, ramen, ramen, ramen],
+  creator: "test",
+  date: "2016-08-20",
+  isVoting: false,
+  name: "test",
+  time: "01:00",
+  users: [],
+  voteCompleted: false
+}]
+
+export default userEvents;
