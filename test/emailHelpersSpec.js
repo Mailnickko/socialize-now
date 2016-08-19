24dc33e7a8ce5ab2d4766fee62bcbb8d00e26959
jtlong3rd <jtlong3rd@gmail.com>
2016-08-19 19:48:47 -0400

    (test) Add a unit test for sendNotification

--- test/emailHelpersSpec.js
+++ test/emailHelpersSpec.js
@@ -1,11 +1,16 @@
 import { sendNotification } from '../notificationHelpers/emailHelpers';
 
+import Promise from 'bluebird';
 import { expect } from 'chai';
 
 describe('emailHelpers', () => {
   describe('sendNotification', () => {
     it('should exist', () => {
       expect(sendNotification).to.be.a.function;
+    });
+
+    it('should return a promise', () => {
+      expect(sendNotification(null, null, '', '') instanceof Promise).to.be.true;
     });
   });
 });
