ac7c41f16e5f4b32725b83253ee41807c8d0fed2
jtlong3rd <jtlong3rd@gmail.com>
2016-08-19 19:48:16 -0400

    (refactor) Have sendNotification return a promise

--- notificationHelpers/emailHelpers.js
+++ notificationHelpers/emailHelpers.js
@@ -1,15 +1,16 @@
 const sendGrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
 const mailHelper = require('sendgrid').mail;
+const Promise = require('bluebird');
 
-// Output: None
+// Output: A promise which resolves to SendGrid's response
 // Side Effects: Sends an email notification to an invitee
 // Input: The event creator and invitee email addresses,
 // the subject, and the email body
 module.exports.sendNotification = (creator, invitee, subject, body) => {
-  from = new mailHelper.Email(creator);
-  to = new mailHelper.Email(invitee);
-  content = new mailHelper.Content('text/plain', body);
-  mail = new mailHelper.Mail(from, subject, to, content);
+  const from = new mailHelper.Email(creator);
+  const to = new mailHelper.Email(invitee);
+  const content = new mailHelper.Content('text/plain', body);
+  const mail = new mailHelper.Mail(from, subject, to, content);
 
   const request = sendGrid.emptyRequest({
     method: 'POST',
@@ -17,11 +18,5 @@ module.exports.sendNotification = (creator, invitee, subject, body) => {
     body: mail.toJSON()
   });
 
-  sendGrid.API(request, (error, response) => {
-    // This can be uncommented for testing purposes
-    // console.log(response.statusCode);
-    // console.log(response.body);
-    // console.log(response.headers);
-  });
+  return Promise.promisify(sendGrid.API)(request).catch();
 };
-
