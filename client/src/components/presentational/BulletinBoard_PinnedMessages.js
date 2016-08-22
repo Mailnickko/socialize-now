5093e07e738862ef6bb6ce30f85387afc2b27f6d
llo1 <louislo@gmail.com>
2016-08-22 15:28:31 -0700

    (feat) Add PinnedMessages component

--- client/src/components/presentational/BulletinBoard_PinnedMessages.js
+++ client/src/components/presentational/BulletinBoard_PinnedMessages.js
@@ -2,14 +2,25 @@ import React, { Component, PropTypes } from 'react';
 import '../../styles/css/polling.css';
 import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router';
+import PinnedMessage from './BulletinBoard_PinnedMessage';
 
 class PinnedMessages extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="winner">
-        <div className="pinnedMessages">
-          <h1>Pinned Messages</h1>
+      <div className="pinnedMessages">
+        <div className="pinnedMessagesContent">
+          <h3>Pinned Messages </h3>
+          <div>
+            <PinnedMessage message="Don't forget to bring your towels" />
+            <PinnedMessage message="Parking is 5 bucks, instructions are at www.parkinghere.com" />
+            <PinnedMessage message="Does anybody have a portable BBQ?" />
+            <PinnedMessage message="Chef Boyardee needs a ride can anybody pick him up at 2938 Ravioli Rd" />
+          </div>
         </div>
       </div>
     );
