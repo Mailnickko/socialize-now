5d842f742810e1595292bd3710e12fecacaa2177
llo1 <louislo@gmail.com>
2016-08-22 13:47:04 -0700

    (feat) Add BulletinBoard component

--- client/src/components/presentational/BulletinBoard.js
+++ client/src/components/presentational/BulletinBoard.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import '../../styles/css/polling.css';
 import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router';
+import EventDetails from './BulletinBoard_EventDetails';
+import PinnedMessages from './BulletinBoard_PinnedMessages';
 
 class BulletinBoard extends Component {
 
@@ -10,23 +12,10 @@ class BulletinBoard extends Component {
   }
 
   render() {
-    const { winner } = this.props;
     return (
-      <div className="winner">
-        <Link to="/dashboard"><button>Back to Dashboard</button></Link>
-        <div className="winnerContent">
-          <h1>
-            <FontAwesome name='sign-language' size='2x' style={{ color: 'white' }} />
-              Winner!
-            <FontAwesome name='sign-language' size='2x' style={{ color: 'white' }} />
-          </h1>
-          <h1>{ winner.choice[0].name }</h1>
-          <div><img className="profilePicture" alt={ winner.choice[0].name } src={ winner.choice[0].imageURL } /></div>
-          <br></br>
-          <button className="infoBtn">
-                <a className="btnLink" href={winner.choice[0].url} target='_blank'>Info</a>
-            </button>
-        </div>
+      <div>
+        <EventDetails winner={this.props.winner} />
+        <PinnedMessages winner={this.props.winner} />
       </div>
     );
   }
