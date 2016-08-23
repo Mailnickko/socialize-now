2c4e45f83fac46a9e23318aae13efa6fe5717f6d
Nicholas Ko <mailnickko@gmail.com>
2016-08-22 21:19:57 -0700

    (refactor) Use path.join to construct path to static files

--- config/middleware.js
+++ config/middleware.js
@@ -1,5 +1,8 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+
 const allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -7,10 +10,10 @@ const allowCrossDomain = function (req, res, next) {
   next();
 };
 
-module.exports = function(app, express) {
+module.exports = function (app, express) {
   app.use(allowCrossDomain);
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../client/build'));
+  app.use(express.static(path.join(__dirname, '/../client/build')));
 };
