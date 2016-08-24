c2f24db87e3ebaef5665188c218a8083dd46a4f5
Minyens <xminhhainguyen@gmail.com>
2016-08-23 23:17:30 -0400

    (feat) Fix voting feature

--- client/src/components/presentational/PollingList.js
+++ client/src/components/presentational/PollingList.js
@@ -33,28 +33,26 @@ class PollingList extends Component {
     let address = nominee.address[2].split(' ').slice(0, -1).join(' ');
     return (
       <div className="nominee">
+        <a href={nominee.url} target='_blank' style={{'margin': 'auto'}}>
+          <button className="infoBtn"></button>
+        </a>
+        <div className="votingInterface">
+          <FontAwesome className="voteArrow" name='arrow-circle-o-down' size='4x' flip='horizontal' style={{ color: 'red' }} onClick={ (e) => this.handleDownVote(e,index, eventId) }/>
+          <div className="votes">
+            <div>{ nominee.netVotes }</div>
+            <div>Current Likes</div>
+          </div>
+          <FontAwesome className="voteArrow" name='arrow-circle-o-up' size='4x' style={{ color: 'green' }} onClick={ (e) => this.handleUpVote(e,index, eventId) }/>
+        </div>
+        <div className="yelpRating">
+          <img src={ nominee.ratingImg } className="yelpStars" alt="nominated-event" />
+          <div className="reviewCount"> ({ nominee.reviewCount })</div>
+        </div>
         <img className='nomineeBG' src={ nominee.imageURL } alt="nominated-event" />
         <div className="nomineeInfo">
           <img className='nomineePic' src={ nominee.imageURL } alt="nominated-event" />
-          <div className="yelpRating">
-            <img src={ nominee.ratingImg } className="yelpStars" alt="nominated-event" />
-            <div className="reviewCount"> ({ nominee.reviewCount })</div>
-          </div>
           <div className="nomineeName">{ nominee.name }</div>
           <div className="nomineeAddress">{ address }</div>
-            <a href={nominee.url} target='_blank' style={{'margin': 'auto'}}>
-              <button className="infoBtn"></button>
-            </a>
-          <div>
-            <div className="votingInterface">
-              <FontAwesome className="voteArrow" name='arrow-circle-o-down' size='4x' flip='horizontal' style={{ color: 'red' }} onClick={ (e) => this.handleDownVote(e,index, eventId) }/>
-              <div className="votes">
-                <div>{ nominee.netVotes }</div>
-                <div>Current Likes</div>
-              </div>
-              <FontAwesome className="voteArrow" name='arrow-circle-o-up' size='4x' style={{ color: 'green' }} onClick={ (e) => this.handleUpVote(e,index, eventId) }/>
-            </div>
-          </div>
         </div>
       </div>
     );
--- client/src/data/event.js
+++ client/src/data/event.js
@@ -9,10 +9,21 @@ const ramen = {
   url: "http://www.yelp.com/biz/toshs-ramen-salt-lake-city-2?adjust_creative=e-QBkPVuSWpweMbvJ7XxnA&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=e-QBkPVuSWpweMbvJ7XxnA"
 }
 
+const ramen2 = {
+  imageURL: "https://s3-media3.fl.yelpcdn.com/bphoto/IhfaY4tDZcd_lM5LEeSYjw/ms.jpg",
+  name: "Tosh's Ramen LONG NAME THAT WILL BREAK THINGS",
+  address:['123 Fake St', 'Fake', 'Fake City, FK 11111'],
+  netVotes: 1,
+  rating: 4,
+  ratingImg: "http://s3-media2.fl.yelpcdn.com/assets/2/www/img/d63e3add9901/ico/stars/v1/stars_large_2_half.png",
+  reviewCount: 302,
+  url: "http://www.yelp.com/biz/toshs-ramen-salt-lake-city-2?adjust_creative=e-QBkPVuSWpweMbvJ7XxnA&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=e-QBkPVuSWpweMbvJ7XxnA"
+}
+
 const event = {
   _id: '12345',
   choice: [],
-  choices: [ramen, ramen, ramen, ramen, ramen, ramen, ramen, ramen],
+  choices: [ramen, ramen2, ramen, ramen, ramen, ramen, ramen, ramen],
   isVoting: true,
   voteCompleted: false
 }
